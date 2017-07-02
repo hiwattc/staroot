ef714da6c6904708baef98d10d4fed2a0af8b375
SEOJUNGBAE <hiwattc@gmail.com>
2017-07-02 21:11:34 +0900

    17.07.02 solving Infinite recursion (StackOverflowError)  with @JsonIgnore in User Class

--- src/main/java/com/staroot/domain/User.java
+++ src/main/java/com/staroot/domain/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -25,7 +25,9 @@ public class User {
 	private String email;
 	
 	//Cascade Issue : http://postitforhooney.tistory.com/entry/JavaJPAHibernate-CascadeType%EB%9E%80-%EA%B7%B8%EB%A6%AC%EA%B3%A0-%EC%A2%85%EB%A5%98
-	@OneToOne(mappedBy="user" ,cascade = {CascadeType.ALL})
+	//@OneToOne(mappedBy="user" ,cascade = {CascadeType.ALL})
+	@OneToOne(mappedBy="user")
+	@JsonIgnore  //solving problem with Infinite recursion (StackOverflowError) :프로필 사진등록된 사용자가 댓글다는 경우 json 무한재귀호출되어 stackoverflow발생
 	private com.staroot.domain.UserPicture file;
 	
 	@JsonProperty
@@ -80,44 +82,6 @@ public class User {
 		return inputPassword.equals(this.password);
 	}
 	
-	@Override
-	public String toString() {
-		return "User [userId=" + userId  + ", name=" + name + ", email=" + email + "]";
-	}
-
-	//이클립스 source > hash&equals 로 자동생성
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		return result;
-	}
-
-	//이클립스 source > hash&equals 로 자동생성
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (userId == null) {
-			if (other.userId != null)
-				return false;
-		} else if (!userId.equals(other.userId))
-			return false;
-		return true;
-	}
-
 	public UserPicture getFile() {
 		return file;
 	}
@@ -125,7 +89,5 @@ public class User {
 	public void setFile(UserPicture file) {
 		this.file = file;
 	}
-	
-	
 
 }
