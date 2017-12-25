1dcf3607238ace3b0b679f452bb3bdc254f18b37
SEOJUNGBAE <hiwattc@gmail.com>
2017-12-25 14:23:18 +0900

    17.12.25 rabbit mq 연동해서 메시지 수신기능(아직은 로그남기는정도) 테스트함

--- src/main/java/com/staroot/StarootApplication.java
+++ src/main/java/com/staroot/StarootApplication.java
@@ -3,8 +3,18 @@ package com.staroot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+
+import com.staroot.util.amqp.*;
 
 
 
@@ -17,6 +27,39 @@ public class StarootApplication {
 	public static String UPLOAD_DIR_CKEDITOR = "/staroot_upload_dir/ckeditor";
 	public static String UPLOAD_DIR_PATH;
 
+	/*rabbit mq test */
+	public final static String queueName = "hello2";
+	@Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("hello2-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }	
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }  
+    
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+    
+    
 	public static void main(String[] args) {
 		SpringApplication.run(StarootApplication.class, args);
 	}
---
+++ src/main/java/com/staroot/util/amqp/Receiver.java
@@ -0,0 +1,31 @@
+package com.staroot.util.amqp;
+
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    
+    
+    public void receiveMessage(Map<String, String> message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+    public void receiveMessage(byte[] message) {
+        System.out.println("Received <" + new String(message) + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
